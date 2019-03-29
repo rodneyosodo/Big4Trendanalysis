4052b0056dcec1b84906e4baab7dd0ed4d6be53e
Rodney Osodo <blackd0t@protonmail.com>
2019-03-29 16:01:32 +0300

    Removing azure key

--- Scripts/crosschecking.py
+++ Scripts/crosschecking.py
@@ -3,24 +3,12 @@ import os
 from pprint import pprint
 import pandas as pd
 import time
+import env
 
-m = 'I had a wonderful experience! The rooms were wonderful and the staff was helpful.'
-key = "2b2422fbccb0407c9294e17cb183b197"
-sentiment_api_url = "http://192.168.99.100:5000/" #text_analytics_base_url + "sentiment"
-headers = {"Ocp-Apim-Subscription-Key": key}
-
-documents = {'documents' : [
-	  {'id': '1', 'language': 'en', 'text': m}
-	]}
-respose = requests.post(sentiment_api_url, headers=headers, json=documents)
-sentiment = respose.json()
-print(sentiment.get('documents')[0].get("score"))
-
-'''
 start = time.time()
-key = "2b2422fbccb0407c9294e17cb183b197"
+key = env.azure_key
 text_analytics_base_url = "https://uksouth.api.cognitive.microsoft.com/text/analytics/v2.0/"
-sentiment_api_url = "http://192.168.99.100:5000/" #text_analytics_base_url + "sentiment"
+sentiment_api_url = text_analytics_base_url + "sentiment"
 headers = {"Ocp-Apim-Subscription-Key": key}
 
 data = pd.read_csv("Data/Crosscheck.csv")
@@ -42,4 +30,3 @@ end = time.time()
 time_diff = end - start
 data['Azuretarget'] = target
 pprint("Finished\nIterations: {}\nLength of data frame: {}\nIt took {} seconds".format(counter, len(data), time_diff))
-'''
