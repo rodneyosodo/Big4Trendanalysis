715f64c8adc63a016269c9c13405288ec29d0eff
Rodney Osodo <blackd0t@protonmail.com>
2019-03-29 11:43:31 +0300

    Modifying streaming service

--- Scripts/twitterstreaming.py
+++ Scripts/twitterstreaming.py
@@ -1,34 +1,48 @@
-import Sentiment_mod as s
-from tweepy import Stream
-from tweepy import OAuthHandler
+#Import the necessary methods from tweepy library
 from tweepy.streaming import StreamListener
-import json
-from http.client import IncompleteRead
+from tweepy import OAuthHandler
+from tweepy import Stream
+import Sentiment as s
 import env
+import json
 
 #consumer key, consumer secret, access token, access secret.
-ckey="gZyYs6aOfVyPixNIEtme2jQAw"
-csecret="VFdtnMVeIdueYNbzrSL1YzS5PYq2df37NvykTGys73VbqihJgx"
-atoken="832625263208914944-BYhciHVLixagRqwhu8DaxwvQpyfPMOD"
-asecret="cdMsYnNxzMRPnDvxSBKExaLdLlCL8ilLC4TmAmxVQixbc"
-class listener(StreamListener):
+ckey=env.API_KEY#"gZyYs6aOfVyPixNIEtme2jQAw"
+csecret=env.API_SECRET_KEY#"VFdtnMVeIdueYNbzrSL1YzS5PYq2df37NvykTGys73VbqihJgx"
+atoken=env.ACCESS_TOKEN#"832625263208914944-BYhciHVLixagRqwhu8DaxwvQpyfPMOD"
+asecret=env.ACCESS_TOKEN_SECRET#"cdMsYnNxzMRPnDvxSBKExaLdLlCL8ilLC4TmAmxVQixbc"
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
     def on_data(self, data):
         try:
-            all_data = json.loads(data)
-            tweet = all_data["text"]
-            sentiment_value,confidence = s.sentiment(tweet)
-            print(tweet, sentiment_value, confidence)
-            if confidence*100 >= 80:
-                output = open("twitter-out.txt", "a")
-                output.write(sentiment_value)
-                output.write("\n")
-                output.close()
+            sentiment_value = s.sentiment(data)
+            print(tweet, sentiment_value)
+            output = open("twitter-out.txt", "a")
+            output.write(sentiment_value)
+            output.write("\n")
+            output.close()
             return True
+            #sentiment_value,confidence = s.sentiment(tweet)
+            #print(tweet, sentiment_value, confidence)
+            #if confidence*100 >= 80:
+            #    output = open("twitter-out.txt", "a")
+            #    output.write(sentiment_value)
+            #    output.write("\n")
+            #    output.close()
+            #return True
         except:
-            return True
+            return True   
         def on_error(self, status):
             print (status)
 
-auth = OAuthHandler(ckey, csecret)
-auth.set_access_token(atoken, asecret)
-twitterStream = Stream(auth, listener()) twitterStream.filter(track=["Trump"])
+if __name__ == '__main__':
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(ckey, csecret)
+    auth.set_access_token(atoken, asecret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=["#Big4Agenda"])
+
