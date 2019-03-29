cb45e190ce69c397548a1ae2e024cd91c2432681
Rodney Osodo <blackd0t@protonmail.com>
2019-03-29 16:02:41 +0300

    Deleting module.py and adding Dataprocessing and sklearn notebooks

--- Scripts/twitterstreaming.py
+++ Scripts/twitterstreaming.py
@@ -1,48 +1,58 @@
-#Import the necessary methods from tweepy library
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
 import Sentiment as s
-import env
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
 import json
+from http.client import IncompleteRead
+import env
 
 #consumer key, consumer secret, access token, access secret.
-ckey=env.API_KEY#"gZyYs6aOfVyPixNIEtme2jQAw"
-csecret=env.API_SECRET_KEY#"VFdtnMVeIdueYNbzrSL1YzS5PYq2df37NvykTGys73VbqihJgx"
-atoken=env.ACCESS_TOKEN#"832625263208914944-BYhciHVLixagRqwhu8DaxwvQpyfPMOD"
-asecret=env.ACCESS_TOKEN_SECRET#"cdMsYnNxzMRPnDvxSBKExaLdLlCL8ilLC4TmAmxVQixbc"
+ckey=env.API_KEY
+csecret=env.API_SECRET_KEY
+atoken= env.ACCESS_TOKEN
+asecret=env.ACCESS_TOKEN_SECRET
 
-#This is a basic listener that just prints received tweets to stdout.
-class StdOutListener(StreamListener):
+
+class listener(StreamListener):
+
     def on_data(self, data):
         try:
-            sentiment_value = s.sentiment(data)
+            all_data = json.loads(data)
+            
+            tweet = all_data["text"]
+            sentiment_value = s.sentiment(tweet)
+
             print(tweet, sentiment_value)
-            output = open("twitter-out.txt", "a")
-            output.write(sentiment_value)
+            output = open("twitter.txt", "a")
+            output.write(str(sentiment_value))
             output.write("\n")
-            output.close()
+            output.close()  
+
             return True
-            #sentiment_value,confidence = s.sentiment(tweet)
-            #print(tweet, sentiment_value, confidence)
-            #if confidence*100 >= 80:
-            #    output = open("twitter-out.txt", "a")
-            #    output.write(sentiment_value)
-            #    output.write("\n")
-            #    output.close()
-            #return True
         except:
-            return True   
-        def on_error(self, status):
-            print (status)
+            return True
 
-if __name__ == '__main__':
-    #This handles Twitter authetification and the connection to Twitter Streaming API
-    l = StdOutListener()
-    auth = OAuthHandler(ckey, csecret)
-    auth.set_access_token(atoken, asecret)
-    stream = Stream(auth, l)
+    def on_error(self, status):
+        print (status)
 
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=["#Big4Agenda"])
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
 
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["#big4agenda"])
+
+"""
+while True:
+    try:
+        # Connect/reconnect the stream
+        twitterStream = Stream(auth, listener())
+        # DON'T run this approach async or you'll just create a ton of streams!
+        twitterStream.filter(track=["Trump"])
+    except IncompleteRead:
+        # Oh well, reconnect and keep trucking
+        continue
+    except KeyboardInterrupt:
+        # Or however you want to exit this loop
+        twitterStream.disconnect()
+        break
+"""
