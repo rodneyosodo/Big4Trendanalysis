c7b2d83bc60222b5cfad6f26ed6bac0b09b2449b
Rodney Osodo <blackd0t@protonmail.com>
2019-03-30 00:13:38 +0300

    Adding API

---
+++ api.py
@@ -0,0 +1,4 @@
+from Scripts import Sentiment as s
+
+print(s.sentiment("This movie was awesome the acting was great, and there were pythons"))
+print(s.sentiment("This movie was utter junk. There were absolutely 0 pythons. I don't see what the point was at all. Horrible movie. 0/10"))
